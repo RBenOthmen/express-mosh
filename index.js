9d0f65e6c7e79dfd953f536177bd69db24bd362b
RBenOthmen <ramibenothmen15@gmail.com>
2024-10-10 12:21:09 +0100

    Handling HTTP POST Requests

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
 const courses = [
     {id: 1, name: 'course1'},
     {id: 2, name: 'course2'},
@@ -21,9 +23,14 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
-// app.get('/api/:year/:month', (req, res) => {
-//     res.send(req.params);
-// });
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
